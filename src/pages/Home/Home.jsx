b5a24d3f062615ebea286e76891f6c9640939bbe
IHAR STALIAROU <devdevgrin@gmail.com>
2025-07-11 15:02:14 +0300

    feat: add functional for sorting and filtering

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,17 +5,26 @@ import { Sort } from '../../components/Sort/Sort'
 import { PizzaBlock } from '../../components/PizzaCard/PizzaCard'
 import { Skeleton } from '../../components/Skeleton/Skeleton'
 
+const BASE_URL = 'https://686d082714219674dcca2427.mockapi.io/pizzas'
+
 export const Home = () => {
   const [pizzas, setPizzas] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [activeCategory, setActiveCategory] = useState(3)
+  const [activeSortType, setActiveSortType] = useState({ name: 'популярности', value: 'rating' })
+  const [sortOrder, setSortOrder] = useState('desc')
+
+  const category = activeCategory > 0 ? `category=${activeCategory}` : ''
+
 
   useEffect(() => {
     getPizzas()
-  }, [])
+  }, [activeCategory, activeSortType, sortOrder])
 
   const getPizzas = async () => {
     try {
-      const request = await fetch('https://686d082714219674dcca2427.mockapi.io/pizzas')
+      setIsLoading(true)
+      const request = await fetch(`${BASE_URL}?${category}&sortBy=${activeSortType.value}&order=${sortOrder}`)
       const data = await request.json()
       setPizzas(data)
       setIsLoading(false)
@@ -26,11 +35,16 @@ export const Home = () => {
       }
     }
   }
+
+  const handleToggleOrder = () => {
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')
+  }
+
   return (
     <>
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories categoryId={activeCategory} onClickCategory={(id) => setActiveCategory(id)} />
+        <Sort sort={activeSortType} order={sortOrder} onChangeSort={(id) => setActiveSortType(id)} onChangeOrder={handleToggleOrder} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
